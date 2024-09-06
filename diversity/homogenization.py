8d691e5db24923e77f6156b6cb222e03e78ce814
Ali Falahati <aflt1998@gmail.com>
2024-09-06 00:17:09 +0000

    remove rounding

--- diversity/homogenization.py
+++ diversity/homogenization.py
@@ -41,7 +41,7 @@ def homogenization_score(
     doc_score = 0
     
     print('==> Scoring all pairs')
-    for pair in tqdm(all_pairs, total=len(data)**2):
+    for pair in tqdm(all_pairs):
         # single document-level homogenization score, pairs are ordered
         if pair[0] == curr_str:     
             doc_score += _calculate_score(pair, scorer, measure, model)
@@ -56,7 +56,7 @@ def homogenization_score(
         corpus_score += doc_score 
     
     # returns corpus level homogenization score 
-    return round(corpus_score / len(all_pairs), 3)
+    return corpus_score / len(all_pairs)
 
 
 @memoized
